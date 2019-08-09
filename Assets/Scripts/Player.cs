f50fbd822b3fdd56b5c10380fbc9e5d1128eedbc
mtlilium <39237030+mtlilium@users.noreply.github.com>
2019-08-09 18:03:38 +0900

    hoge

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+	Vector3 goal = new Vector3(30,201,-4);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		GetComponent<NavMeshAgent>().SetDestination(goal);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
