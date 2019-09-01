83945938891ce630e203144240365866e73fef4c
o-atsu <a0t5s0u1@gmail.com>
2019-09-02 00:57:17 +0900

    Making_0902_1
    NPC.prefabの作成

--- Assets/Scripts/ConversationClass.cs
+++ Assets/Scripts/ConversationClass.cs
@@ -10,11 +10,11 @@ public class ConversationClass
     [SerializeField]
     List<Saying> Contents = new List<Saying>();//会話の内容
 
-    IEnumerator Speak(){//スペースキーが押されると会話が進む
+    IEnumerator Talk(){//スペースキーが押されると会話が進む
     	for(int i = 0;i < Contents.Count;i++){
 	    Debug.Log(Contents[i].Who + " " + Contents[i].What);
-	    yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
 	    yield return null;
+	    yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
 	}
 	yield break;
     }
@@ -22,7 +22,7 @@ public class ConversationClass
 
 [System.SerializableAttribute]
 class Saying{//インスペクタから表示、編集する内容を示すクラス
-    public int Who;
+    public int Who = 0;
     [Multiline(3)]
-    public string What;
+    public string What = "default";
 }
