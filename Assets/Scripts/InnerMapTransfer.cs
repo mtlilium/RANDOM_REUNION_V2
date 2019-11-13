10db2c421c971c3d579fa54334fe58463d987906
o-atsu <a0t5s0u1@gmail.com>
2019-11-13 15:55:37 +0900

    1113_1
    ひな形の追加

---
+++ Assets/Scripts/InnerMapTransfer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InnerMapTransfer : MonoBehaviour
+{
+	void OnTriggerEnter(Collider other){
+		Debug.Log("transfer now");
+	}
+}
