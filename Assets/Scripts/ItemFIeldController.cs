4958571cc4f2cb7b46fc16969a65b3971832ae24
Matsu-Y74 <37435149+Matsu-Y74@users.noreply.github.com>
2019-11-18 01:25:06 +0900

    NameModification
    誤字があったため修正

--- Assets/Scripts/ItemFIeldController.cs
+++ Assets/Scripts/ItemFIeldController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemFIeldController : MonoBehaviour
+public class ItemFieldController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
