19bc2150b4c14a7c215fd2ba629d32425c74d4e1
o-atsu <a0t5s0u1@gmail.com>
2019-11-22 01:38:13 +0900

    1122_1
    スクリプトの実装
    ※テスト用のシーンをSceneディレクトリに残しておきます

--- Assets/Scripts/InnerMapTransfer.cs
+++ Assets/Scripts/InnerMapTransfer.cs
@@ -4,7 +4,28 @@ using UnityEngine;
 
 public class InnerMapTransfer : MonoBehaviour
 {
-	void OnTriggerEnter(Collider other){
-		Debug.Log("transfer now");
+	private GameObject ParentGrid;//オブジェクトがGridの子であるとする
+	private GameObject NextMap;
+	public bool warped = false;//無限ワープ防止
+	[SerializeField]
+	private InnerMapTransfer NextPoint = null;
+	
+	void OnTriggerEnter2D(Collider2D other){
+		if(warped) return;
+		//Debug.Log("transfer now");
+		ParentGrid.SetActive(false);
+		NextPoint.warped = true;
+		NextMap.SetActive(true);
+		other.gameObject.transform.parent = NextMap.transform;
+		other.gameObject.transform.position = NextPoint.transform.position;
 	}
+
+	void OnTriggerExit2D(){
+		warped = false;
+	}
+	void Awake(){
+		ParentGrid = transform.root.gameObject;
+		NextMap = NextPoint.transform.root.gameObject;
+	}
+
 }
