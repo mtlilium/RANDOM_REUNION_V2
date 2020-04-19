5e139ad8e0888c673b0187ab345f9bcbb78169ef
unknown <haan314@outlook.jp>
2020-04-19 16:36:46 +0900

    Add_PalameterScriptAndStatusScript
    prefabのplayerにのみアタッチ

---
+++ Assets/Scripts/StatusScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatusScript : MonoBehaviour
+{
+    bool isInvincible;
+    public void Eninvincible() {
+        isInvincible = true;
+    }
+}
