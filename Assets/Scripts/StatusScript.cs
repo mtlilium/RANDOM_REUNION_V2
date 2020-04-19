1ff1bf5cd1b26163cf91a32ae52a8f70087e7202
unknown <haan314@outlook.jp>
2020-04-19 18:17:26 +0900

    AtackHitTest
    敵の攻撃が当たったときにDebug.Logが出るように

---
+++ Assets/Scripts/StatusScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatusScript : MonoBehaviour
+{
+    public bool IsInvincible {//無敵状態かどうか
+        get;
+        private set;
+    }
+    public void EnInvincible() {
+        IsInvincible = true;
+    }
+    
+}
