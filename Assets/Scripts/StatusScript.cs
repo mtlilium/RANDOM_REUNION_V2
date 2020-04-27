b3bfc0e638941283ecb98a492541bbe915a1dee4
unknown <haan314@outlook.jp>
2020-04-27 22:25:22 +0900

    AddInvincibleAnimation
    ・点滅するInvincibleアニメーションを追加　animationの仕様が分からず上手く時間が合わせられない
    ・デバッグ用にMouseの攻撃animationをいじって孔隙判定が永続的に発生するように

--- Assets/Scripts/StatusScript.cs
+++ Assets/Scripts/StatusScript.cs
@@ -4,20 +4,22 @@ using UnityEngine;
 
 public class StatusScript : MonoBehaviour
 {
+    Animator invincibleAnimator;
+    private void Start() {
+        invincibleAnimator = this.GetComponent<Animator>();
+    }
     public bool IsInvincible {//無敵状態かどうか
         get;
         private set;
     }
     public void EnInvincible() {
         IsInvincible = true;
+        invincibleAnimator?.SetTrigger("Invincible");
         StartCoroutine(InvincibleEndCoroutine());
     }
     IEnumerator InvincibleEndCoroutine() {
         yield return new WaitForSeconds(1.0f);
         IsInvincible = false;
-    }
-    private void Update() {
-        
     }
 
 }
