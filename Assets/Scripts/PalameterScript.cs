4a69bd9aa6dbe1363704cd5e6a0c21e11cd36482
unknown <haan314@outlook.jp>
2020-05-11 18:59:25 +0900

    Add_AudioClipPlayer
    ・AudioClipをInspectorから指定して流せるようにAudioClipPlayerを追加
    ・Player等にアタッチ

--- Assets/Scripts/PalameterScript.cs
+++ Assets/Scripts/PalameterScript.cs
@@ -6,7 +6,13 @@ public class PalameterScript : MonoBehaviour
 {
     [SerializeField]
     int hp;//初期値はInspectorから
+
+    AudioClipPlayer audioClipPlayer;
+    private void Start() {
+        audioClipPlayer = GetComponent<AudioClipPlayer>();
+    }
     public void GainDamage(int damage) {
+        //audioClipPlayer.Play("damage");
         hp-=damage;
         Debug.Log(damage + "ダメージ");
     }
---
+++ Assets/Scripts/Sound/AudioClipPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioClipPlayer : MonoBehaviour
+{
+    [SerializeField]
+    List<AudioClip> clipList;
+
+    Dictionary<string, AudioClip> clipDict;
+
+    AudioSource source;
+    private void Start() {
+        source = GetComponent<AudioSource>();
+        if (source == null) {
+            Debug.Log(gameObject.name+"にAudioSourceがありません");
+        }
+        clipDict = new Dictionary<string, AudioClip>();
+        foreach(var clip in clipList) {
+            clipDict.Add(clip.name, clip);
+        }
+    }
+    public void Play(string audioName) {
+        if (!clipDict.ContainsKey(audioName)) {
+            Debug.Log(audioName + "という名前のAudioClipがclipDict内に見つかりません");
+            return;
+        }
+        source.PlayOneShot(clipDict[audioName]);
+    }
+}
