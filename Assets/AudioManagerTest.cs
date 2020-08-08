3070d7f30f95f58ab03ac07f3e8c026e3468bb91
mtlilium <39237030+mtlilium@users.noreply.github.com>
2020-08-08 14:33:39 +0900

    ini

---
+++ Assets/AudioManagerTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManagerTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioMixer audioMixer;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeSEVol()
+    {
+        audioMixer.SetFloat("vol_SE", -40f);
+    }
+}
