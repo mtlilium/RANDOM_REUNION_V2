f351036fe1f33da515875a32363d69377bc3ceaf
unknown <haan314@outlook.jp>
2021-01-17 21:26:13 +0900

    random-chang_animated
    ・らんだむちゃんがアニメーションするように
    ・moveanimatedをオンにするとアニメーションのStateが方向に応じて切り替わるように
    制御はhttp://dlod.hatenablog.com/entry/2018/07/28/225247 参考
    ・アニメーションを自動生成してStateも自動設定できるようにしたい

--- Assets/Scripts/MovableObjectScript.cs
+++ Assets/Scripts/MovableObjectScript.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 
 public class MovableObjectScript : ObjectOnMapScript{
     Rigidbody2D rb2d;
-
+    [SerializeField]
     float movement = 0.1f;
     directions _state;
     directions state {
@@ -40,6 +40,8 @@ public class MovableObjectScript : ObjectOnMapScript{
 
     Dictionary<directions, Sprite> stateDic = new Dictionary<directions, Sprite>();
 
+    Dictionary<directions, Vector2> stateToXYDic = new Dictionary<directions, Vector2>();
+
     public Action<directions> ActionsWhenDirecrionChanged;
 
     public new void Awake()
@@ -63,9 +65,17 @@ public class MovableObjectScript : ObjectOnMapScript{
         ActionsWhenDirecrionChanged += LookAtDirection;
         if (moveAnimated) {
             animator = GetComponent<Animator>();
-            ActionsWhenDirecrionChanged += (newDir) => {
-                if(newDir!=directions.Undefined) animator.CrossFadeInFixedTime(Enum.GetName(typeof(directions), newDir), 0);
+            ActionsWhenDirecrionChanged += (dir) => {
+                if (dir != directions.Undefined) animator.CrossFadeInFixedTime(Enum.GetName(typeof(directions), dir), 0);
             };
+            stateToXYDic.Add(directions.Up,         new Vector2( 0, 1));
+            stateToXYDic.Add(directions.UpRight,    new Vector2( 1, 1));
+            stateToXYDic.Add(directions.Right,      new Vector2( 1, 0));
+            stateToXYDic.Add(directions.DownRight,  new Vector2( 1,-1));
+            stateToXYDic.Add(directions.Down,       new Vector2( 0,-1));
+            stateToXYDic.Add(directions.DownLeft,   new Vector2(-1,-1));
+            stateToXYDic.Add(directions.Left,       new Vector2(-1, 0));
+            stateToXYDic.Add(directions.UpLeft,     new Vector2(-1, 1));
         }
         else {
             ActionsWhenDirecrionChanged += (newDir) => {
@@ -74,20 +84,18 @@ public class MovableObjectScript : ObjectOnMapScript{
         }
     }
 
-    Vector2 prePosition;
-    bool isIdle=false;
+    bool moved = false;
+    bool isIdle = false;
     protected void Update() {
         if (moveAnimated) {
-            /*
-            if (prePosition == rb2d.position && !isIdle) {
-                DebugLogWrapper.Log("Changed to Idle");
-                animator.CrossFadeInFixedTime("Idle", 0);
-                sr.sprite = stateDic[state];
+            if (moved) {
+                isIdle = false;
+            }
+            else if (!isIdle) {
+                animator.CrossFadeInFixedTime("Idle_Blend", 0);
                 isIdle = true;
             }
-            if (prePosition != rb2d.position) isIdle = false;
-            prePosition = rb2d.position;
-            */
+            moved = false;
         }
     }
 
@@ -97,9 +105,21 @@ public class MovableObjectScript : ObjectOnMapScript{
     }
     public void Move(Vector2 direction, float q)//引数の方向に移動量Qだけ移動
     {
-        rb2d.MovePosition(rb2d.position + direction.normalized * q);
+        Vector2 normalizedDir = direction.normalized;
+        rb2d.MovePosition(rb2d.position + normalizedDir * q);
         directions dir = DirectionOfDeltaPos(direction);
         if (state!=dir) ActionsWhenDirecrionChanged(dir);
+        if (moveAnimated) {
+            if (direction != Vector2.zero) moved = true; //動いていたらtrue 動いてない場合にanimationを停止させるための処理
+            if (dir != directions.Undefined) {
+                Vector2 laseMove = stateToXYDic[dir];
+                animator.SetFloat("LastMoveX", laseMove.x);
+                animator.SetFloat("LastMoveY", laseMove.y);
+                if (isIdle) {
+                    animator.CrossFadeInFixedTime("Walk_Blend", 0);
+                }
+            }
+        }
     }
     
     public void Move(MapCoordinate mapcoordinate)//MapCoordinateのToVector2の方向に移動量movementだけ移動
